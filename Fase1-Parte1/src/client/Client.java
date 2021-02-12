a619563fd2a994304be95f195ca38031724f295d
albuana <aalbuquerque1999@hotmail.com>
2021-02-12 20:38:04 +0000

    Client done

--- Fase1-Parte1/src/client/Client.java
+++ Fase1-Parte1/src/client/Client.java
@@ -13,18 +13,16 @@ public final class Client {
 	private static Client INSTANCE = null;
 	private static ObjectOutputStream out;
 	private static ObjectInputStream in;
-	private static String userId;
+	private static String userID;
 	private static String password;
 	private static String serverAddress;
+
 
 	/**
 	 * Returns the Client singleton instance
 	 * @return Client singleton
 	 */
 	public static Client getInstance() {
-		if(INSTANCE == null)
-			INSTANCE = new Client();
-		
 		return INSTANCE;
 	}
 	
@@ -33,30 +31,7 @@ public final class Client {
 	 * @param server address serverAddress
 	 * @param user
 	 */
-	private Client() {
-	
-	}
-	/**
-	 * 
-	 * @param serverAddress
-	 * @param userId
-	 * @param password
-	 */
-	public void setInfo(String serverAddress, String userId, String password) {
-		Client.password = password;
-		Client.serverAddress = serverAddress;
-		Client.userId = userId;		
-		
-	}
-
-	/**
-	 * Returns the Client singleton instance after creating the Client and connecting to the server
-	 * @param userID 
-	 * @param server address serverAddress
-	 * @return Client singleton
-	 */
-	public static Client connect(String serverAddress, String userID) {
-		//NAO E NECESSARIO USERID
+	private Client(String serverAddress, String userID) {
 		String[] infoList = serverAddress.split(":", 2);
 
 		String ipServer = infoList[0];
@@ -73,11 +48,21 @@ public final class Client {
 			Client.out = new ObjectOutputStream(Client.socket.getOutputStream());
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}	
 		
-		
+	}
+	
+	/**
+	 * Returns the Client singleton instance after creating the Client and connecting to the server
+	 * @param userID 
+	 * @param server address serverAddress
+	 * @return Client singleton
+	 */
+	public static Client connect(String serverAddress, String userID) {
+		INSTANCE = new Client(serverAddress, userID);
 		return INSTANCE;
 	}
+	
 
 	/**
 	 * Sends data
@@ -129,7 +114,7 @@ public final class Client {
 	 * @return UserID
 	 */
 	public String getUserID() {
-		return Client.userId;
+		return Client.userID;
 	}
 	
 	public String getServerAddress() {
