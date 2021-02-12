5a407a06b013c8180eb78242e5d2b59cd49056da
albuana <aalbuquerque1999@hotmail.com>
2021-02-12 17:43:15 +0000

    Client Main

--- Fase1-Parte1/src/client/Client.java
+++ Fase1-Parte1/src/client/Client.java
@@ -1,4 +1,4 @@
-package server;
+package client;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
--- Fase1-Parte1/src/client/handler/LoginUserHandler.java
+++ Fase1-Parte1/src/client/handler/LoginUserHandler.java
@@ -1,5 +1,20 @@
 package client.handler;
 
 public class LoginUserHandler {
+	
+	private String userID;
+	private String pass;
+
+	/**
+	 * 
+	 * @param userID the user's username
+	 * @param password the user's password
+	 */
+	public LoginUserHandler(String userID, String pass) {
+		this.userID = userID;
+		this.pass = pass;
+	}
+	
+	
 
 }
