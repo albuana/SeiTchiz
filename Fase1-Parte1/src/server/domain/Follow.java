95feb076a548758724085fb127436c7e37950619
Gonçalo <fc52831@alunos.fc.ul.pt>
2021-02-24 00:43:42 +0000

    Correcao Follow

--- Fase1-Parte1/src/server/domain/Follow.java
+++ Fase1-Parte1/src/server/domain/Follow.java
@@ -41,44 +41,48 @@ public class Follow {
 	 */
 	public String follow(User userID, String currentUserID) throws IOException  {
 
-		if(verifyUserExist(userID) && verifyUserFollowingHimSelf(userID, currentUserID)) {
-			File dir = new File(FOLLOWS_DIRECTORY);
-			File[] directoryListing = dir.listFiles();
+		if(!verifyUserExist(userID))
+			return "O user nao existe";
+		if(!verifyUserFollowingHimSelf(userID, currentUserID)) 
+			return "O user nao se pode seguir a si proprio";
 
-			//Iterate on directory follows
-			for(File userFile: directoryListing) {
+		File dir = new File(FOLLOWS_DIRECTORY);
+		File[] directoryListing = dir.listFiles();
 
-				//Checks if any file has his name
-				if(userFile.getName().equals(currentUserID+".txt")){
+		//Iterate on directory follows
+		for(File userFile: directoryListing) {
 
-					Scanner sc = new Scanner(userFile);
+			//Checks if any file has his name
+			if(userFile.getName().equals(currentUserID+".txt")){
 
-					//Search through the file
-					while (sc.hasNextLine()) {
-						String line = sc.nextLine();
+				Scanner sc = new Scanner(userFile);
 
-						//Checks if userID is already being followed
-						if(line.equals(userID.getUsername())) {
-							sc.close();
-							return userID.getUsername()+" já esta a ser seguido/a";
-						}
+				//Search through the file
+				while (sc.hasNextLine()) {
+					String line = sc.nextLine();
+
+					//Checks if userID is already being followed
+					if(line.equals(userID.getUsername())) {
+						sc.close();
+						return userID.getUsername()+" já esta a ser seguido/a";
 					}
-					
-					//Write on file if the user to follow is not already being followed
-					Files.write(userFile.toPath(), (userID.getUsername()+"\n").getBytes(), StandardOpenOption.APPEND);
-					sc.close();
-					return userID.getUsername()+" foi seguido/a";
 				}
 
+				//Write on file if the user to follow is not already being followed
+				Files.write(userFile.toPath(), (userID.getUsername()+"\n").getBytes(), StandardOpenOption.APPEND);
+				sc.close();
+				return userID.getUsername()+" foi seguido/a";
 			}
-			//At this point it has been conclude that the current user does not have a file
 
-			File newFile = new File(FOLLOWS_DIRECTORY+currentUserID+".txt");
-			newFile.createNewFile();
-			Files.write(newFile.toPath(), (userID.getUsername()+"\n").getBytes(), StandardOpenOption.APPEND);
-			return userID.getUsername()+" foi seguido/a";
 		}
+		//At this point it has been conclude that the current user does not have a file
+
+		File newFile = new File(FOLLOWS_DIRECTORY+currentUserID+".txt");
+		newFile.createNewFile();
+		Files.write(newFile.toPath(), (userID.getUsername()+"\n").getBytes(), StandardOpenOption.APPEND);
 		return userID.getUsername()+" foi seguido/a";
+
+
 	}
 
 	/**
@@ -90,44 +94,49 @@ public class Follow {
 	 */
 	public String unfollow(User userID, String currentUserID) throws IOException  {
 
-		if(verifyUserExist(userID) && verifyUserFollowingHimSelf(userID, currentUserID)) {
+		if(!verifyUserExist(userID))
+			return "O user nao existe";
+		if(!verifyUserFollowingHimSelf(userID, currentUserID)) 
+			return "O user nao se pode seguir a si proprio";
 
 
-			File dir = new File(FOLLOWS_DIRECTORY);
-			File[] directoryListing = dir.listFiles();
-			StringBuilder newFile = new StringBuilder();
-			boolean deleted = false;
+		File dir = new File(FOLLOWS_DIRECTORY);
+		File[] directoryListing = dir.listFiles();
+		StringBuilder newFile = new StringBuilder();
+		boolean deleted = false;
 
-			//Iterate on directory follows
-			for(File userFile: directoryListing) {
-				
-				//Checks if any file has his name
-				if(userFile.getName().equals(currentUserID+".txt")){
+		//Iterate on directory follows
+		for(File userFile: directoryListing) {
 
-					Scanner sc = new Scanner(userFile);
-					
-					//Search through the file
-					while (sc.hasNextLine()) {
-						String line = sc.nextLine();
+			//Checks if any file has his name
+			if(userFile.getName().equals(currentUserID+".txt")){
 
-						//Delete line
-						if(line.equals(userID.getUsername()) && !deleted) {
-							deleted=true;
-						}else {
-							newFile.append(line+"\n");
-						}
+				Scanner sc = new Scanner(userFile);
+
+				//Search through the file
+				while (sc.hasNextLine()) {
+					String line = sc.nextLine();
+
+					//Delete line
+					if(line.equals(userID.getUsername()) && !deleted) {
+						deleted=true;
+					}else {
+						newFile.append(line+"\n");
 					}
-
-					sc.close();
-					
-					//Rewrite the file without the use to unfollow
-					Files.write(userFile.toPath(), newFile.toString().getBytes());
-
-					if(deleted) 
-						return userID.getUsername()+" já nao esta a ser seguido/a";
 				}
+
+				sc.close();
+
+				//Rewrite the file without the use to unfollow
+				Files.write(userFile.toPath(), newFile.toString().getBytes());
+
+				if(deleted) 
+					return userID.getUsername()+" já nao esta a ser seguido/a";
+				else
+					return "O user nao segue "+userID.getUsername();
 			}
-			return "O user nao tem seguidores"; 
+
+
 		}
 		return "O user nao tem seguidores";
 	}
@@ -137,7 +146,7 @@ public class Follow {
 	 * @throws FileNotFoundException 
 	 */
 	public String viewFollowers(String userID) throws FileNotFoundException {
-		
+
 		StringBuilder ret = new StringBuilder();
 		File dir = new File(FOLLOWS_DIRECTORY);
 		File[] directoryListing = dir.listFiles();
@@ -146,14 +155,14 @@ public class Follow {
 
 		//Iterate on directory follows
 		for(File userFile: directoryListing) {
-			
+
 			String fname = userFile.getName();
 			int pos = fname.lastIndexOf(".");
 			fname = fname.substring(0, pos);
 
 			if(!fname.equals(userID)) {
 				Scanner sc = new Scanner(userFile);
-				
+
 				//Search through the file
 				while(sc.hasNextLine()) {
 					String line = sc.nextLine();
@@ -161,7 +170,7 @@ public class Follow {
 						ret.append(fname+", ");
 					}
 				}
-				
+
 				sc.close();
 			}
 		}
@@ -173,9 +182,9 @@ public class Follow {
 		return ret.toString();
 	}
 
-	
-	
-	
+
+
+
 	private boolean verifyUserExist(User userID) throws IOException {
 		//This condition verifies if the user exists
 		if(userID == null || UserCatalog.getInstance().getUser(userID.getUsername())==null) 
